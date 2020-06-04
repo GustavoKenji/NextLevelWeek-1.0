d13f233c3f81a1cd164a014d0e038643dd8688e6
Gustavo Kenji <gustavokomatsu4@gmail.com>
2020-06-03 23:58:23 -0300

    aula2 - back-end building

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  console.log('Listagem de Usuários');
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-  res.json([
-    'Diego',
-    'Kenji',
-    'WebMaster'
-  ]);
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
